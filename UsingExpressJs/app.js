2ecde4234a14fd0c0255f7204b7a77369c308bf1
Pranav <pranav.developer@outlook.com>
2020-04-13 01:35:14 +0530

    Filtering Paths

--- UsingExpressJs/app.js
+++ UsingExpressJs/app.js
@@ -6,7 +6,11 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use('/', shopRoutes);
+
+app.use((req,res, next) => {
+    res.status(404).send('<h1>Page Not Found</h1>');
+})
 
 app.listen(3000);
--- UsingExpressJs/routes/admin.js
+++ UsingExpressJs/routes/admin.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 });
 
 router.post('/product', (req, res , next) => {
