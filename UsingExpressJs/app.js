56637cf01eddd09c32c981bdd984851da6862976
Pranav <pranav.developer@outlook.com>
2020-04-13 01:07:41 +0530

    Added body-parser to get request body from post method

--- UsingExpressJs/app.js
+++ UsingExpressJs/app.js
@@ -1,16 +1,22 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware');
-    next();
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
 });
 
-app.use((req, res, next) => {
+app.use('/product', (req, res , next) => {
+    console.log(req.body);
+    res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
     console.log('In the another middleware');
 });
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
